1daf05c6690751054cb52f02d7747f9b84b4bb77
watomon <watomon1@gmail.com>
2018-01-05 19:55:28 +0900

    commit

---
+++ codingbat-warmup-1-backAround.java
@@ -0,0 +1,24 @@
+/*
+Given a string, take the last char and return a new string with
+the last char added at the front and back, so "cat" yields "tcatt".
+The original string will be length 1 or more.
+
+backAround("cat") Å® "tcatt"
+backAround("Hello") Å® "oHelloo"
+backAround("a") Å® "aaa"
+
+*/
+
+public String backAround(String str) {
+    char last = str.charAt(str.length()-1);
+    return last + str + last;
+  }
+  
+  /*
+   answer
+   public String backAround(String str) {
+    // Get the last char
+    String last = str.substring(str.length() - 1);
+    return last + str + last;
+   }
+  */
