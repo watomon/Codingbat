459c220039ad69865e78ab96b2119c165541084d
watomon <watomon1@gmail.com>
2018-01-05 16:35:28 +0900

    commit

---
+++ codingbat-warmup-1-frontBack.java
@@ -0,0 +1,19 @@
+public String frontBack(String str) {
+    String str2;
+    char first,last;
+    
+    if (str.length()<=1){
+      return str;
+    }
+    
+    first = str.charAt(str.length()-1);
+    last = str.charAt(0);
+    str2 = str.substring(1, str.length()-1);
+    
+    return first+str2+last;
+    /*
+    String mid = str.substring(1, str.length()-1);
+    // last + mid + first
+    return str.charAt(str.length()-1) + mid + str.charAt(0);
+    */
+  }
