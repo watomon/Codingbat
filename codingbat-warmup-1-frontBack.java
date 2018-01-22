788825912ff114ff893f91bbe3d09d02c465a938
watomon <watomon1@gmail.com>
2018-01-22 23:54:45 +0900

    fix indent

--- codingbat-warmup-1-frontBack.java
+++ codingbat-warmup-1-frontBack.java
@@ -11,9 +11,10 @@ public String frontBack(String str) {
     str2 = str.substring(1, str.length()-1);
     
     return first+str2+last;
-    /*
+    
+}
+/* Solution
     String mid = str.substring(1, str.length()-1);
     // last + mid + first
     return str.charAt(str.length()-1) + mid + str.charAt(0);
-    */
-}
+*/
